5c11d722e32eab8765f3a3e58ac9743d4bd78082
Abhishek Mallick <mallickabhishek97@gmail.com>
2023-12-07 20:12:32 +0530

    logo

--- query-builder/client/src/App.jsx
+++ query-builder/client/src/App.jsx
@@ -1,10 +1,28 @@
 import { useState } from 'react'
+import styles from './styles.module.css'
+import databaseLogo from './assets/database-storage.png'
+
 import './App.css'
 
 function App() {
 
   return (
-    <h1>Decision Hub</h1>
+    <main className={styles.main}>
+          <img src={databaseLogo} 
+          alt="Database Logo" 
+          className={styles.icon}
+          style={{ width: '100px', height: '100px' }} 
+          />
+          <h1>Decision Hub</h1>
+          <form>
+            <input 
+              type="text"
+              name="query-descriptor"
+              placeholder="Describe your query"
+              />
+          </form>
+    </main>
+
   )
 }
 

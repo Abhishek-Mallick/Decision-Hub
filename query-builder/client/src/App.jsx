ccbfbf6986a56c45430111e0d2e42cff692bffe7
Abhishek Mallick <mallickabhishek97@gmail.com>
2023-12-15 23:16:59 +0530

    fix(query-builder/server): Updated API endpoints

--- query-builder/client/src/App.jsx
+++ query-builder/client/src/App.jsx
@@ -71,13 +71,14 @@ const [copySuccess, setCopySuccess] = useState(false);
   
 
   return (
-    <main className={styles.main} style={{ backgroundImage: "url('https://images.unsplash.com/photo-1613946709284-0aa74f040a12?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')" }}>
+    // <main className={styles.main} style={{ backgroundImage: "url('https://images.unsplash.com/photo-1613946709284-0aa74f040a12?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')" }}>
+    <main className={styles.main}>
           <img src={databaseLogo} 
           alt="Database Logo" 
           className={styles.icon}
           style={{ width: '100px', height: '100px' }} 
           />
-          <h1 style={{color: 'white'}}>Decision Hub</h1>
+          <h1 style={{color: 'black'}}>Decision Hub</h1>
           <form onSubmit={onSubmit}>
             <input 
               type="text"

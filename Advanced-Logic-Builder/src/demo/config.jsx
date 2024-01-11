bdf1f95a264a97f9787f426af6c6ac1d67ff41e6
Abhishek Mallick <mallickabhishek97@gmail.com>
2024-01-12 03:18:24 +0530

    feat: Added advanced control logic

---
+++ Advanced-Logic-Builder/src/demo/config.jsx
@@ -0,0 +1,282 @@
+import React from "react";
+import merge from "lodash/merge";
+import { BasicConfig } from "@react-awesome-query-builder/ui";
+const InitialConfig = BasicConfig;
+
+
+//////////////////////////////////////////////////////////////////////
+
+const fields = {
+  user: {
+    label: "User",
+    tooltip: "Group of fields",
+    type: "!struct",
+    subfields: {
+      firstName: {
+        label2: "Username", //only for menu's toggler
+        type: "text",
+        mainWidgetProps: {
+          valueLabel: "Name",
+          valuePlaceholder: "Enter name",
+        },
+        fieldSettings: {
+          validateValue: (val, fieldSettings) => {
+            return (val.length < 10);
+          },
+        },
+      },
+      login: {
+        type: "text",
+        fieldSettings: {
+          validateValue: (val, fieldSettings) => {
+            return (val.length < 10 && (val === "" || val.match(/^[A-Za-z0-9_-]+$/) !== null));
+          },
+        },
+        mainWidgetProps: {
+          valueLabel: "Login",
+          valuePlaceholder: "Enter login",
+        },
+      }
+    }
+  },
+  num: {
+    label: "Number",
+    type: "number",
+    preferWidgets: ["number"],
+    fieldSettings: {
+      min: -1,
+      max: 5
+    },
+  },
+  slider: {
+    label: "Slider",
+    type: "number",
+    preferWidgets: ["slider", "rangeslider"],
+    valueSources: ["value", "field"],
+    fieldSettings: {
+      min: 0,
+      max: 100,
+      step: 1,
+      marks: {
+        0: <strong>0%</strong>,
+        100: <strong>100%</strong>
+      },
+    },
+    //overrides
+    widgets: {
+      slider: {
+        widgetProps: {
+          valuePlaceholder: "..Slider",
+        }
+      }
+    },
+  },
+  date: {
+    label: "Date",
+    type: "date",
+    valueSources: ["value"],
+  },
+  time: {
+    label: "Time",
+    type: "time",
+    valueSources: ["value"],
+    operators: ["greater_or_equal", "less_or_equal", "between"],
+    defaultOperator: "between",
+  },
+  datetime: {
+    label: "DateTime",
+    type: "datetime",
+    valueSources: ["value"]
+  },
+  datetime2: {
+    label: "DateTime2",
+    type: "datetime",
+    valueSources: ["field"]
+  },
+  color: {
+    label: "Color",
+    type: "select",
+    valueSources: ["value"],
+    fieldSettings: {
+      listValues: [
+        { value: "yellow", title: "Yellow" },
+        { value: "green", title: "Green" },
+        { value: "orange", title: "Orange" }
+      ],
+    }
+  },
+  color2: {
+    label: "Color2",
+    type: "select",
+    fieldSettings: {
+      listValues: {
+        yellow: "Yellow",
+        green: "Green",
+        orange: "Orange",
+        purple: "Purple"
+      },
+    }
+  },
+  multicolor: {
+    label: "Colors",
+    type: "multiselect",
+    fieldSettings: {
+      listValues: {
+        yellow: "Yellow",
+        green: "Green",
+        orange: "Orange"
+      },
+      allowCustomValues: true
+    },
+  },
+  stock: {
+    label: "In stock",
+    type: "boolean",
+    defaultValue: true,
+    mainWidgetProps: {
+      labelYes: "+",
+      labelNo: "-"
+    }
+  },
+};
+
+
+//////////////////////////////////////////////////////////////////////
+
+const conjunctions = {
+  AND: InitialConfig.conjunctions.AND,
+  OR: InitialConfig.conjunctions.OR,
+};
+
+const operators = {
+  ...InitialConfig.operators,
+  // examples of  overriding
+  between: {
+    ...InitialConfig.operators.between,
+    textSeparators: [
+      "from",
+      "to"
+    ],
+  },
+};
+delete operators.proximity;
+
+const widgets = {
+  ...InitialConfig.widgets,
+  // examples of  overriding
+  slider: {
+    ...InitialConfig.widgets.slider,
+    customProps: {
+      width: "300px"
+    }
+  },
+  rangeslider: {
+    ...InitialConfig.widgets.rangeslider,
+    customProps: {
+      width: "300px"
+    }
+  },
+  date: {
+    ...InitialConfig.widgets.date,
+    dateFormat: "DD.MM.YYYY",
+    valueFormat: "YYYY-MM-DD",
+  },
+  time: {
+    ...InitialConfig.widgets.time,
+    timeFormat: "HH:mm",
+    valueFormat: "HH:mm:ss",
+  },
+  datetime: {
+    ...InitialConfig.widgets.datetime,
+    timeFormat: "HH:mm",
+    dateFormat: "DD.MM.YYYY",
+    valueFormat: "YYYY-MM-DD HH:mm:ss",
+  },
+};
+
+
+const types = {
+  ...InitialConfig.types,
+  // examples of  overriding
+  boolean: merge({}, InitialConfig.types.boolean, {
+    widgets: {
+      boolean: {
+        widgetProps: {
+          hideOperator: true,
+          operatorInlineLabel: "is",
+        }
+      },
+    },
+  }),
+};
+
+
+const localeSettings = {
+  locale: {
+    moment: "ru",
+  },
+  valueLabel: "Value",
+  valuePlaceholder: "Value",
+  fieldLabel: "Field",
+  operatorLabel: "Operator",
+  fieldPlaceholder: "Select field",
+  operatorPlaceholder: "Select operator",
+  deleteLabel: null,
+  addGroupLabel: "Add group",
+  addRuleLabel: "Add rule",
+  addSubRuleLabel: "Add sub rule",
+  delGroupLabel: null,
+  notLabel: "Not",
+  fieldSourcesPopupTitle: "Select source",
+  valueSourcesPopupTitle: "Select value source",
+  removeRuleConfirmOptions: {
+    title: "Are you sure delete this rule?",
+    okText: "Yes",
+    okType: "danger",
+  },
+  removeGroupConfirmOptions: {
+    title: "Are you sure delete this group?",
+    okText: "Yes",
+    okType: "danger",
+  },
+};
+
+const settings = {
+  ...InitialConfig.settings,
+  ...localeSettings,
+
+  valueSourcesInfo: {
+    value: {
+      label: "Value"
+    },
+    field: {
+      label: "Field",
+      widget: "field",
+    },
+  },
+  // canReorder: false,
+  // canRegroup: false,
+  // showNot: false,
+  // showLabels: true,
+  maxNesting: 3,
+  canLeaveEmptyGroup: true, //after deletion
+
+};
+
+const funcs = {};
+
+const ctx = InitialConfig.ctx;
+
+const config = {
+  ctx,
+  conjunctions,
+  operators,
+  widgets,
+  types,
+  settings,
+  fields,
+  funcs
+};
+
+export default config;
+
